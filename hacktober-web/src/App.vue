28f83dd0fe55f7025cbd606addae5ebd0dcf2c81
Taylor Jackson <tjtay2008@gmail.com>
2020-10-21 09:23:10 -0500

    Add Frontend

---
+++ hacktober-web/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app">
+    <Home title="Hacking the Tobers!"/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+.app {
+  font-family: Roboto, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #000;
+}
+</style>
---
+++ hacktober-web/src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img 
+      alt="Hacktober Pumpkin"
+      class='home_image'
+      src="../assets/hacktober-pumpkin.jpeg"
+    >
+    <h1 class="home_title">{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+}
+.home_title {
+  font-size: 96px;
+  color: #F75F1C;
+  width: 100%;
+}
+.home_image {
+  align-self: center;
+  height: auto;
+  max-width: 300px;
+  width: 100%;
+}
+</style>
