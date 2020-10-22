41efb8a8128af0b21f6ac4325b36004b8a929d6a
LaDfBC <george.mausshardt@mst.edu>
2020-10-21 21:26:14 -0500

    Updating config fetcher

--- src/main/python/config/configuration.py
+++ src/main/python/config/configuration.py
@@ -1,7 +1,10 @@
 from configparser import ConfigParser
+import pathlib
+import os
 
 # Look, this isn't the best but I'm going for speed right now rather than enterprise integrity
-DEVELOPMENT_CONFIG_FILE_PATH = "/home/george/htbdev.cfg"
+DEVELOPMENT_CONFIG_FILE_PATH = str(pathlib.Path(__file__).parent.parent.parent.parent.parent) \
+                               + os.sep + "resources" + os.sep + "htbdev.cfg"
 
 def fetch_database_configs(config_path):
     config = ConfigParser()
