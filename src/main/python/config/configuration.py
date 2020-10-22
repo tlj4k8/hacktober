36221b7bac014895e7480b1b1299edba91621efd
LaDfBC <george.mausshardt@mst.edu>
2020-10-21 21:55:45 -0500

    Adding ROOT_DIR parameter

--- src/main/python/config/configuration.py
+++ src/main/python/config/configuration.py
@@ -1,10 +1,8 @@
 from configparser import ConfigParser
-import pathlib
+from src import constants
 import os
 
-# Look, this isn't the best but I'm going for speed right now rather than enterprise integrity
-DEVELOPMENT_CONFIG_FILE_PATH = str(pathlib.Path(os.path.abspath(__file__)).parent.parent.parent.parent.parent) \
-                               + os.sep + "resources" + os.sep + "htbdev.cfg"
+DEVELOPMENT_CONFIG_FILE_PATH = str(constants.ROOT_DIR) + os.sep + "resources" + os.sep + "htbdev.cfg"
 
 def fetch_database_configs(config_path):
     config = ConfigParser()
